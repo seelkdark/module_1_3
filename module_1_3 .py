23e0cb3a5c707fea4f724dbbd742614dc9d11e92
seelk <sedova08111984@gmail.com>
2024-10-13 23:57:29 +0300

    Update module_1_3 .py

--- module_1_3 .py
+++ module_1_3 .py
@@ -3,6 +3,7 @@ print('Name:' + name)
 age = 18
 print('Age:' , age)
 age = age+1
-print('New age:' , age+1)
-is_student = age == age
+new_age = 19
+print('New age:' , age)
+is_student = new_age == age
 print('Is student' , is_student)
